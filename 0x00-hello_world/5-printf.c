317e9dfd41c68c06f1961a32227290f941e064f7
AbduLLAHI-Knowledge_addict <murabiusman@gmail.com>
2022-06-09 14:41:19 -0700

    sucide

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * This nigger should always return zero okay
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
