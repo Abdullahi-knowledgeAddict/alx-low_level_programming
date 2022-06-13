6d34caea74082a93adc141321b86531789d2881b
AbduLLAHI-Knowledge_addict <murabiusman@gmail.com>
2022-06-13 08:58:10 -0700

    lower case alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print all letters of the alphabets
+ * using for loop.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c < 'z'; c++)
+		putchar(c);
+	return (0);
+}
