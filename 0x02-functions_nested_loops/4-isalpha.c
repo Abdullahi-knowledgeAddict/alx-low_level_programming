63e880c611e0fb9a0494d4ed48a6d246d682885e
AbduLLAHI-Knowledge_addict <murabiusman@gmail.com>
2022-06-18 02:00:28 -0700

    4-isalpha.c: checks for alphabets in a giving parameter & it has been declared in main.h

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,26 @@
 #include "main.h"
 /**
- * _isalpha - a function that checks for alphabetic character
- * @c: single letter input
- * Return: 1 if c is a letter (lower or uppercase), 0 otherwise
+ * _isalpha - checks if a given parameter is an alphabet.
+ * @c: the parameter to be inspected.
+ *
+ * Return: 1 or 0 if parameter is alphabetic or not , repectively.
  */
 int _isalpha(int c)
 {
-	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
-		return (1);
-	else
-		return (0);
+	int a;
+	int A;
+	int result;
+
+	for (a = 'a', A = 'A'; A <= 'Z'; a++, A++)
+	{
+		if (c == a || c == A)
+		{
+			result = 1;
+			break;
+		}
+		else
+			result = 0;
+	}
+	return (result);
 }
+
