1d6d86668b4e2ca61e44e6e2dfb3f03ae278a29a
AbduLLAHI-Knowledge_addict <murabiusman@gmail.com>
2022-06-19 04:25:17 -0700

    prins time tab 0-9x

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,35 +1,29 @@
 #include "main.h"
+
 /**
- * times_table - a function that prints the 9 times table, starting with 0
- * rone = row, cone = column, d = digits of current result
- * Return: times table
- * add extra space past single digit
+ * times_table - returns the times table upto 9.
+ *
+ * Return: always zero success.
  */
 void times_table(void)
 {
-	int rone, cone, d;
+	int a;
+	int b;
 
-	for (rone = 0; rone <= 9; rone++)
+	for (a = 0; a <= 9 ; a++)
 	{
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		for (cone = 1; cone <= 9; cone++)
+		for (b = 0; b <= 9; b++)
 		{
-			d = (rone * cone);
-			if ((d / 10) > 0)
+			if ((a * b) > 9)
 			{
-				_putchar((d / 10) + '0');
+				_putchar(((a * b) / 10) + '0');
+				_putchar(((a * b) % 10) + '0');
+				_putchar(' ');
 			}
 			else
 			{
+				_putchar((a * b) + '0');
 				_putchar(' ');
-			}
-			_putchar((d % 10) + '0');
-
-			if (cone < 9)
-			{
-				_putchar(',');
 				_putchar(' ');
 			}
 		}
