aa2f83b58108cae888cd5d3a8d94ac0083678c59
USMAN SAYEDI ALIYU <usmanmurabi@gmail.com>
2023-02-02 10:18:41 +0100

    Include Macros in your C - source code

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Prints the name of the file of the program
- *        was compiled from, followed by a new line.
- *
- * Return: Always 0.
+ * main - a program that prints the name of the program it was compiled from
+ * Return: always zero success
  */
 int main(void)
 {
 	printf("%s\n", __FILE__);
-
 	return (0);
 }
