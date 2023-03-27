f12f5090502995866b7eaea69ae90ae5f79f90f6
MUROBI <usmanmurabi@gmail.com>
2023-03-27 09:59:43 +0100

    more algorithms for singly linked list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -4,27 +4,33 @@
  * reverse_listint - Reverses a listint_t list.
  * @head: A pointer to the address of
  *        the head of the list_t list.
+ *I wish to further simplify this code, but due to time i can't for now!
  *
  * Return: A pointer to the first node of the reversed list.
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *ahead, *behind;
+	listint_t *next, *prev;
+	int i;
 
-	if (head == NULL || *head == NULL)
-		return (NULL);
-
-	behind = NULL;
-
-	while ((*head)->next != NULL)
+	i = 0;
+	next = (*head)->next;
+	while(next)
 	{
-		ahead = (*head)->next;
-		(*head)->next = behind;
-		behind = *head;
-		*head = ahead;
+		prev = *head;
+		if(i == 0){
+			*head = (*head)->next;
+			next = (*head)->next;
+			(*head)->next = prev;
+			prev->next = NULL;
+			i = 1;
+		}
+		else{
+			*head = next;
+			next = (*head)->next;
+			(*head)->next = prev;
+		}
 	}
-
-	(*head)->next = behind;
 
 	return (*head);
 }
