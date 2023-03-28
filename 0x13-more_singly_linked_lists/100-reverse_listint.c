6491798b2c88e79b474f0ee0f67bca90f8307769
MUROBI <usmanmurabi@gmail.com>
2023-03-28 18:18:41 +0100

    declare 2 variables max

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -13,7 +13,7 @@ listint_t *reverse_listint(listint_t **head)
 	listint_t *next, *prev;
 
 	prev = next = NULL;
-	if (head && *head)
+	if (head && *head && (*head)->next)
 	{
 		prev = *head;
 		*head = (*head)->next;
