d05c0627bbf4bc65514ce7966f96843e151dd0fd
MUROBI <usmanmurabi@gmail.com>
2023-03-28 17:28:15 +0100

    avoid segmentation fault

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,8 +6,11 @@
  */
 void free_list(list_t *head)
 {
-	if (head->next)
-		free_list(head->next);
-	free(head->str);
-	free(head);
+	if (head)
+	{
+		if (head->next)
+			free_list(head->next);
+		free(head->str);
+		free(head);
+	}
 }
